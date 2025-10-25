42b567ed4e616d1abe6cbf5d8bd0db332f4d3bff
eyadmotawe <eyadmotawe@gmail.com>
2025-10-25 15:14:32 +0300

    added GPAPAnel and fixed layout

--- Student Management System/src/StudentManagementSystem/AddStudentPanel.java
+++ Student Management System/src/StudentManagementSystem/AddStudentPanel.java
@@ -10,44 +10,59 @@ public class AddStudentPanel extends JPanel implements ActionListener {
     AddStudentPanel() {
         // Name field
         JPanel NamePanel = new JPanel();
-        NamePanel.setSize(300, 50);
+        NamePanel.setSize(30, 50);
         JLabel Name = new JLabel("Name");
-        Name.setSize(150, 30);
         NamePanel.add(Name);
         JTextField NameField = new JTextField();
-        NameField.setColumns(75);
+        NameField.setColumns(30);
         NamePanel.add(NameField);
-        NameField.setPreferredSize(new Dimension(100, 75));
+        NameField.setPreferredSize(new Dimension(30, 30));
 
         // Age field
         JPanel AgePanel = new JPanel();
         JLabel age = new JLabel("Age");
         AgePanel.add(age);
         JTextField AgeField = new JTextField();
-        AgeField.setColumns(75);
-        AgeField.setPreferredSize(new Dimension(100, 75));
+        AgeField.setColumns(30);
+        AgeField.setPreferredSize(new Dimension(30, 30));
         AgePanel.add(AgeField);
 
         // Gender field
         JPanel GenderPanel = new JPanel();
+        JLabel genderLabel = new JLabel("Gender");
+        GenderPanel.add(genderLabel);
         JComboBox<String> gender = new JComboBox<>();
         gender.setModel(new DefaultComboBoxModel<>(new String[] { "Male", "Female" }));
         GenderPanel.add(gender);
+
 
         // Department field
         JPanel DepartmentPanel = new JPanel();
         JTextField departmentField = new JTextField();
         JLabel department = new JLabel("Department");
         DepartmentPanel.add(department);
-        departmentField.setColumns(75);
-        departmentField.setPreferredSize(new Dimension(200, 75));
+        departmentField.setColumns(30);
+        departmentField.setPreferredSize(new Dimension(30, 30));
         DepartmentPanel.add(departmentField);
 
-        setLayout(new GridLayout(0, 1, 10, 0));
+        // GPA field
+        JPanel GPAPanel = new JPanel();
+        JLabel GPA = new JLabel("GPA");
+        GPAPanel.add(GPA);
+        JTextField GPAField = new JTextField();
+        GPAField.setColumns(30);
+        GPAField.setPreferredSize(new Dimension(30, 30));
+        GPAPanel.add(GPAField);
+
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        setAlignmentY(Component.TOP_ALIGNMENT);
+        setAlignmentX(Component.LEFT_ALIGNMENT);
+
         this.add(NamePanel);
-        this.add(AgePanel);
+        this.add(AgePanel );
         this.add(GenderPanel);
         this.add(DepartmentPanel);
+        this.add(GPAPanel);
     }
 
     @Override
