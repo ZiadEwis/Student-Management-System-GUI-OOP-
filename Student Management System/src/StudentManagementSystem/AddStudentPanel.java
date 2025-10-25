f04f93714a33df3eb71396619834fee18c9e5fae
eyadmotawe <eyadmotawe@gmail.com>
2025-10-25 14:45:26 +0300

    added gender ComboBox

--- Student Management System/src/StudentManagementSystem/AddStudentPanel.java
+++ Student Management System/src/StudentManagementSystem/AddStudentPanel.java
@@ -1,19 +1,43 @@
 package StudentManagementSystem;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddStudentPanel extends JPanel implements ActionListener {
 
     AddStudentPanel() {
+
+        // Name field
         JPanel NamePanel = new JPanel();
+        NamePanel.setSize(300, 50);
         JLabel Name = new JLabel("Name");
+        Name.setSize(150, 30);
         NamePanel.add(Name);
         JTextField NameField = new JTextField();
+        NameField.setColumns(75);
         NamePanel.add(NameField);
-        NamePanel.setSize(300, 100);
+        NameField.setPreferredSize(new Dimension(100, 75));
+
+        // Age field
+        JPanel AgePanel = new JPanel();
+        JLabel age = new JLabel("Age");
+        AgePanel.add(age);
+        JTextField AgeField = new JTextField();
+        AgeField.setColumns(75);
+        AgeField.setPreferredSize(new Dimension(100, 75));
+        AgePanel.add(AgeField);
+
+        // Gender field
+        JPanel GenderPanel = new JPanel();
+        JComboBox<String> gender = new JComboBox<>();
+        gender.setModel(new DefaultComboBoxModel<>(new String[] { "Male", "Female" }));
+        GenderPanel.add(gender);
+        setLayout(new GridLayout(2, 1, 10, 1));
         this.add(NamePanel);
+        this.add(AgePanel);
+        this.add(GenderPanel);
     }
 
     @Override
